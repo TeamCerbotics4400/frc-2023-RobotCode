c7ea3188b3733d9795dcc56c8c239c4d115f7d39
DiegoLolzano <diegolozanor66@gmail.com>
2023-01-10 18:18:53 -0600

    Initial branch commit

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -5,9 +5,6 @@
 package frc.robot;
 
 import frc.robot.Constants.OperatorConstants;
-import frc.robot.commands.Autos;
-import frc.robot.commands.ExampleCommand;
-import frc.robot.subsystems.ExampleSubsystem;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import edu.wpi.first.wpilibj2.command.button.Trigger;
@@ -20,7 +17,6 @@ import edu.wpi.first.wpilibj2.command.button.Trigger;
  */
 public class RobotContainer {
   // The robot's subsystems and commands are defined here...
-  private final ExampleSubsystem m_exampleSubsystem = new ExampleSubsystem();
 
   // Replace with CommandPS4Controller or CommandJoystick if needed
   private final CommandXboxController m_driverController =
@@ -43,12 +39,11 @@ public class RobotContainer {
    */
   private void configureBindings() {
     // Schedule `ExampleCommand` when `exampleCondition` changes to `true`
-    new Trigger(m_exampleSubsystem::exampleCondition)
-        .onTrue(new ExampleCommand(m_exampleSubsystem));
+  
 
     // Schedule `exampleMethodCommand` when the Xbox controller's B button is pressed,
     // cancelling on release.
-    m_driverController.b().whileTrue(m_exampleSubsystem.exampleMethodCommand());
+    
   }
 
   /**
@@ -58,6 +53,6 @@ public class RobotContainer {
    */
   public Command getAutonomousCommand() {
     // An example command will be run in autonomous
-    return Autos.exampleAuto(m_exampleSubsystem);
+    return null;
   }
 }
--- src/main/java/frc/robot/subsystems/DriveTrain.java
+++ src/main/java/frc/robot/subsystems/DriveTrain.java
@@ -4,18 +4,140 @@
 
 package frc.robot.subsystems;
 
+import com.ctre.phoenix.motorcontrol.TalonSRXSimCollection;
+import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
+import com.ctre.phoenix.sensors.BasePigeonSimCollection;
+import com.ctre.phoenix.sensors.Pigeon2;
 import com.revrobotics.CANSparkMax;
 import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.kinematics.DifferentialDriveOdometry;
+import edu.wpi.first.wpilibj.Encoder;
+import edu.wpi.first.wpilibj.RobotBase;
+import edu.wpi.first.wpilibj.RobotController;
+import edu.wpi.first.wpilibj.drive.DifferentialDrive;
+import edu.wpi.first.wpilibj.motorcontrol.MotorControllerGroup;
+import edu.wpi.first.wpilibj.simulation.DifferentialDrivetrainSim;
+import edu.wpi.first.wpilibj.simulation.EncoderSim;
+import edu.wpi.first.wpilibj.smartdashboard.Field2d;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class DriveTrain extends SubsystemBase {
   /** Creates a new DriveTrain. */
-  CANSparkMax LeftFrMotor = new CANSparkMax(1, MotorType.kBrushless);
-  public DriveTrain() {}
+  WPI_TalonSRX leftLeader = new WPI_TalonSRX(0);
+  WPI_TalonSRX leftFollower = new WPI_TalonSRX(1);
+  WPI_TalonSRX rightLeader = new WPI_TalonSRX(2);
+  WPI_TalonSRX rightFollower = new WPI_TalonSRX(3);
+
+  TalonSRXSimCollection leftSim = leftLeader.getSimCollection();
+  TalonSRXSimCollection rightSim = rightLeader.getSimCollection();
+
+  private final MotorControllerGroup m_leftGroup = 
+  new MotorControllerGroup(leftLeader, leftFollower);
+
+  private final MotorControllerGroup m_rightGroup =
+  new MotorControllerGroup(rightLeader, rightFollower);
+
+  private final DifferentialDrive m_drive = new DifferentialDrive(m_leftGroup, m_rightGroup);
+
+  private Encoder m_leftEncoder = new Encoder(0, 1, true);
+
+  private Encoder m_rightEncoder = new Encoder(0, 1, false);
+
+  private Pigeon2 m_gyro = new Pigeon2(0);
+
+  private final DifferentialDriveOdometry m_odometry;
+
+  private DifferentialDrivetrainSim m_drivetrainSim;
+
+  private EncoderSim m_leftEncoderSim;
+  private EncoderSim m_rightEncoderSim;
+
+  private Field2d m_fieldSim;
+
+  BasePigeonSimCollection m_gyroSim;
+
+  public DriveTrain() {
+    
+    m_leftEncoder.setDistancePerPulse(0);
+    m_rightEncoder.setDistancePerPulse(0);
+
+    leftLeader.configFactoryDefault();
+    rightLeader.configFactoryDefault();
+
+    leftFollower.configFactoryDefault();
+    rightFollower.configFactoryDefault();
+
+    m_odometry = new 
+    DifferentialDriveOdometry(Rotation2d.fromDegrees(getHeading()), 
+    0, 0);
+
+    if(RobotBase.isSimulation()){
+      
+    }
+
+    m_leftEncoderSim = new EncoderSim(m_leftEncoder);
+    m_rightEncoderSim = new EncoderSim(m_rightEncoder);
+
+    m_gyroSim = m_gyro.getSimCollection();
+
+    m_fieldSim = new Field2d();
+  }
 
   @Override
   public void periodic() {
     // This method will be called once per scheduler run
+    m_odometry.update(Rotation2d.fromDegrees(getHeading()),
+     m_leftEncoder.getDistance(), m_rightEncoder.getDistance());
+
+    SmartDashboard.putData("Field", m_fieldSim);
+    
   }
+
+  @Override
+  public void simulationPeriodic(){
+    m_drivetrainSim.setInputs(m_leftGroup.get() * RobotController.getBatteryVoltage(), 
+    -m_rightGroup.get() * RobotController.getBatteryVoltage());
+    m_drivetrainSim.update(0.020);
+
+    m_leftEncoderSim.setDistance(m_drivetrainSim.getLeftPositionMeters());
+    m_leftEncoderSim.setRate(m_drivetrainSim.getLeftVelocityMetersPerSecond());
+    m_rightEncoderSim.setDistance(m_drivetrainSim.getRightPositionMeters());
+    m_rightEncoderSim.setRate(m_drivetrainSim.getRightVelocityMetersPerSecond());
+
+    m_gyroSim.setRawHeading(-m_drivetrainSim.getHeading().getDegrees());
+  }
+
+  //Odometry
+  public Pose2d getPose(){
+    return m_odometry.getPoseMeters();
+  }
+
+  public double getHeading(){
+    return Math.IEEEremainder(m_gyro.getYaw(), 360);
+  }
+
+  public void resetOdometry(Pose2d pose){
+    m_drivetrainSim.setPose(pose);
+    m_odometry.resetPosition(Rotation2d.fromDegrees(getHeading()), 
+    0, 0, pose);
+
+  }
+
+  public void resetEncoders(){
+    m_leftEncoder.reset();
+    m_rightEncoder.reset();
+  }
+
+  public void zeroHeading(){
+    
+  }
+
+  public void setMaxOutput(double maxOutput){
+    m_drive.setMaxOutput(maxOutput);
+  }
+
 }
