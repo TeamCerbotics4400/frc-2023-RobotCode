f417232f8bab524950d8accf9d043c3b077c5e22
DiegoLolzano <diegolozanor66@gmail.com>
2023-01-11 20:02:19 -0600

    Codigo limpiado

--- src/main/java/frc/robot/subsystems/DrivetrainSim.java
+++ src/main/java/frc/robot/subsystems/DrivetrainSim.java
@@ -34,7 +34,6 @@ import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.RamseteCommand;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
-import frc.robot.Constants.DriveConstants;
 import frc.robot.Constants.SimulationConstants;
 
 //26 de largo
